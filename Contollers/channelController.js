10fbd7355f07b038fb25bc9271171a2444595c33
pabloelias2000 <equipodeapoyo54@gmail.com>
2024-11-12 12:24:47 -0400

    controller

---
+++ Contollers/channelController.js
@@ -0,0 +1,86 @@
+import Channel from "../Models/Channel";
+
+const channelController = {
+getAllChannels: async (request, response, next) => {
+let channels;
+let error = null;
+let success= true;
+try {
+    channels = await Channel.find();
+    response.status(201).json({
+        response: channels
+    })
+    } catch (error) {
+    response.status(500).json({ error})
+     }
+    },
+
+ getOneChannel: async (request, response, next) => {
+    console.log(request.params);
+    const {id}= request.params;
+    console.log(id);
+    let channel;
+    let error = null;
+    let success= true;
+    try {
+      channel = await Channel.findById({_id:id});
+    } catch (e) {
+        console.log(e);
+        success = false;
+        error = e;
+    }
+    response.json(
+        {
+            response: channel,
+            success,
+            error
+
+
+        }
+    );
+
+},
+
+createChannel: async (request, response, next) => { try {
+    const channel = await Channel.create(request.body);
+    response.status(201).json({
+        response: channel
+    })} catch (err) {
+        response.status(500).json({ error: err })
+    }
+},
+
+updateChannel: async (request, response, next) => { 
+    const { id } = request.params;
+    let channel;
+    let error = null;
+    let success= true;
+    try {
+        channel = await Channel.findByIdAndUpdate({_id: id}, request.body, { new: true });
+    } catch (e) {
+        console.log(e);
+        success = false;
+        error = e;
+    }
+    response.json(
+        {
+            response: channel,
+            success,
+            error
+        }
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+}
